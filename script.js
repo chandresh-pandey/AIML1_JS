73e68dc4febbec7ce4128e74f7ee182f2c6fc74e
chandresh-pandey <chandreshpandey605@gmail.com>
2026-02-20 10:31:28 +0530

    script

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // console.log("hello world");
 //  document.write('hello world');
- var num= 10;
- console.log(num);
- console.log(typeof(num));
- num = false;
- console.log(num);
- console.log(typeof(num));
- num ="Sanjeev";
- console.log(num);
- console.log(typeof(num));
- num = null;
- console.log(num);
- console.log(typeof(num));
- num = undefined;
- console.log(num);
- console.log(typeof(num)); 
+
+
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 =Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
