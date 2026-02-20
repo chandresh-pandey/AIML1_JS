f141dad54cfd3b18e07841cc23a0690f4945ba6c
chandresh-pandey <chandreshpandey605@gmail.com>
2026-02-20 10:10:46 +0530

    script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// console.log("hello world");
+//  document.write('hello world');
+ var num= 10;
+ console.log(num);
+ console.log(typeof(num));
+ num = false;
+ console.log(num);
+ console.log(typeof(num));
+ num ="Sanjeev";
+ console.log(num);
+ console.log(typeof(num));
+ num = null;
+ console.log(num);
+ console.log(typeof(num));
+ num = undefined;
+ console.log(num);
+ console.log(typeof(num)); 
