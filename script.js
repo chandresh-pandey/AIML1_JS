686295cd9190015d40a31967d385899200e8f0d9
chandresh-pandey <chandreshpandey605@gmail.com>
2026-03-09 15:55:03 +0530

    refference

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-// console.log("hello world");
-//  document.write('hello world');
+// // console.log("hello world");
+// //  document.write('hello world');
 
 
-// const sym1 = Symbol(4);
-// console.log(sym1);
-// const sym2 =Symbol(4);
-// console.log(sym2);
-// if(sym1==sym2){
-//     console.log("true");
-// }
-// else{
-//     console.log("false");
-// }
-// var a = 10;
-// var b = 20;
-// var linebreak = "<br/>";
-// document.write("(a<b)=>");
-// result = (a<b);
-// document.write(result);
-// var count;
-// document.write("starting loop" +"<br/>");
-// for(count=0;count<10;count++){
-//     document.write("current count :"+count);
-//     document.write("<br/>");
-// }
-const person={
-    fname:"ajay",lname:"singh",age:25
-};
-for(let x in person){
-    console.log("person details:"+x+":"+person[x])
-}
+// // const sym1 = Symbol(4);
+// // console.log(sym1);
+// // const sym2 =Symbol(4);
+// // console.log(sym2);
+// // if(sym1==sym2){
+// //     console.log("true");
+// // }
+// // else{
+// //     console.log("false");
+// // }
+// // var a = 10;
+// // var b = 20;
+// // var linebreak = "<br/>";
+// // document.write("(a<b)=>");
+// // result = (a<b);
+// // document.write(result);
+// // var count;
+// // document.write("starting loop" +"<br/>");
+// // for(count=0;count<10;count++){
+// //     document.write("current count :"+count);
+// //     document.write("<br/>");
+// // }
+// // const person={
+// //     fname:"ajay",lname:"singh",age:25
+// // };
+// // for(let x in person){
+// //     console.log("person details:"+x+":"+person[x])
+// // }
+// var myinfo = new Array();
+// var myinfo1 = Array();
+// console.log(myinfo,myinfo1);
+// let collection =[
+//     {},
+//     [],
+//     true,
+//     "john",
+//     function(){},
+//     90008428,
+//     undefined,
+//     null,
+//     new String("abc"),
+//     new Date(),
+// ];
+// console.log(collection);
+// console.log(collection[collection.length-1]);
+// console.log(collection.length); 
+const result = document.getElementById("para");
+result.innerHTML= "my self";
+result.title = "myname";
+console.log(result);
+const result2 = document.getElementsByClassName("heading");
+result2.innerHTML="hii";
+console.log(result2);
