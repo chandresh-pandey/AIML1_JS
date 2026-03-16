66aea693f2fdfbea766dc1e6ef9347ca1b1c336a
chandresh-pandey <chandreshpandey605@gmail.com>
2026-03-16 15:40:22 +0530

    events

---
+++ reference.js
@@ -0,0 +1,18 @@
+// document.getElementsByClassName("");
+//  const element = document.getElementById("myh1");
+//  let text = element.getAttribute("class");
+//  function myfunction(){
+//     document.getElementById("myh1").setAttribute("class","democlass");
+//  }
+//  document.getElementById("myh1").
+// create element
+// const heading = document.createElement("h2");
+// // get the parent
+// const bodytag = document.getElementsByTagName("body");
+// bodytag[0].append(heading
+function onMouseover(){
+    console.log("on Mouse Over")
+}
+function myFunction(){
+    alert("web page loaded successufly")
+}
